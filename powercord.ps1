41672d1d16c250f403e80e3085169acf4e0ad609
joshmanjb <joshuabutcher862@gmail.com>
2021-09-10 16:12:11 -0700

    Created script

--- powercord.ps1
+++ powercord.ps1
@@ -1,1 +1,16 @@
-Set-Location $env:USERPROFILE
+# Self-elevate the script if required
+#if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+#    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+#     $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+#     Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+#     Exit
+#    }
+#   }
+#
+Set-Location $env:USERPROFILE\Documents
+Get-Package -Provider chocolatey -Force
+Install-Package nodejs npm git discord-canary -Force -ProviderName chocolatey
+git clone https://github.com/powercord-org/powercord
+Set-Location .\powercord
+npm i
+npm run plug
