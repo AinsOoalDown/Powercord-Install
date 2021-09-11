e48a14e66be668065bd64fcbed4c05b6b32b278c
AinsOoalDown <joshuabutcher862@gmail.com>
2021-09-10 20:40:25 -0700

    E

--- powercord.ps1
+++ powercord.ps1
@@ -1,12 +1,4 @@
-# Self-elevate the script if required
-#if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-#    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-#     $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-#     Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-#     Exit
-#    }
-#   }
-#
+
 Set-Location $env:USERPROFILE\Documents
 Get-Package -Provider chocolatey -Force
 Install-Package nodejs npm git discord-canary -Force -ProviderName chocolatey
@@ -14,5 +6,15 @@ git clone https://github.com/powercord-org/powercord
 Set-Location .\powercord
 npm i
 npm run plug
+
+ Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+     $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+     Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+     Exit
+    }
+   }
 pip3 install PyGithub requests
 pip install GitPython
+.\Install_Plugins.py
