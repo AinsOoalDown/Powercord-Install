1bc44050244f5e9e1a3c497c69b1e9385bac5f47
joshmanjb <joshuabutcher862@gmail.com>
2021-10-19 11:33:58 -0700

    Comment

--- powercord.ps1
+++ powercord.ps1
@@ -1,12 +1,6 @@
+#Get script location
 $location = Get-Location
-#Self-elevate the script if required
-#if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-#    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-#     $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-#     Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-#     Exit
-#    }
-#   }
+
 #move to documents
 Set-Location $env:USERPROFILE\Documents
 Get-Package -Provider chocolatey -Force
@@ -31,7 +25,8 @@ pip3 install PyGithub requests
 #install GitPython
 pip install GitPython
 
-#install plugins
+#Set location back to original
 Set-Location $location
 
+#Run python plugin installer
 Start-Process .\Install_Plugins.py
